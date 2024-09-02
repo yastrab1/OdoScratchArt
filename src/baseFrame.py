8251b4768fc50c0e2cb9d9863ef2866c055c23b9
Luki <lipka.luki@gmail.com>
2024-09-03 00:10:16 +0200

    Filesystem refactoring + added FLUX.1 dev image generator for baseFrame

--- src/baseFrame.py
+++ src/baseFrame.py
@@ -1,14 +1,20 @@
 ﻿import random
-
+import os
+from dotenv.main import load_dotenv
 import cv2
 from PIL import Image
-
+import requests
 import ImageUtils
+
+import io
 
 
 class BaseFrame:
     def __init__(self,firstFrame,mixer):
-        self.img = ImageUtils.pilToCv2(Image.open("test.webp"))
+        self.img = ImageUtils.pilToCv2(self.generateImage(self.generatePrompt()))
+        cv2.imshow("Skbidi",self.img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
         self.curFrame = 0
 
     def generatePrompt(self):
@@ -29,6 +35,21 @@ class BaseFrame:
         prompt += " " + ", ".join([f"{p} {s}" for p, s in zip(promptPony, promptState)])
 
         return prompt
+
+    def generateImage(self,prompt):
+        load_dotenv(verbose=True)
+        os.environ["HUGGING_API"] = "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"
+
+        payload = {
+            "inputs": prompt,
+        }
+
+        API_URL = "https://api-inference.huggingface.co/models/black-forest-labs/FLUX.1-dev"
+        headers = {"Authorization": f"Bearer {os.getenv("HUGGING_API")}"}
+
+        response = requests.post(API_URL, headers=headers, json=payload)
+
+        return Image.open(io.BytesIO(response.content))
 
 
     def baseImg(self,frame):
